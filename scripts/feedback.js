21c47c9b99ec1d3e4d2e8256760d232fbfa5256f
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2024-12-05 20:13:54 +0700

    ...

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,47 +1,36 @@
 const containerRatingStarEls = [...document.querySelectorAll('.container-rating-star')];
+const containerLessonFeedBackEl = document.querySelector('.container-lesson-feedback');
+const containerMenuItemFeedbackEls = document.querySelectorAll('.container-menu-item-feedback');
 
 containerRatingStarEls.forEach(star => {
     star.addEventListener('click', () => {
 
+        containerLessonFeedBackEl.classList.add('lower-part');
+
         containerRatingStarEls.forEach(el => el.classList.remove('selected-star'));
         star.classList.add('selected-star');
 
-        const starIndex = [...containerRatingStarEls].indexOf(star);
+        const starIndex = containerRatingStarEls.indexOf(star);
 
         containerRatingStarEls.forEach((el, index) => {
            el.classList.toggle('color-rating-star', index <= starIndex);
-        });
 
+            if (starIndex <= 2) {
+                containerMenuItemFeedbackEls[0].classList.remove('negative');
+                containerMenuItemFeedbackEls[1].classList.add('positive');
+            } else {
+                containerMenuItemFeedbackEls[0].classList.add('negative');
+                containerMenuItemFeedbackEls[1].classList.remove('positive');
+            }
+        });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const lessonFeedBackSummaryEls = document.querySelectorAll('.lesson-feedback-summary');
 
 lessonFeedBackSummaryEls.forEach(lesson => {
     lesson.addEventListener('click', () => {
-        const isActive = lesson.classList.contains('active');
-
-        if (!isActive) {
-            lesson.classList.add('active');
-        } else {
-            lesson.classList.remove('active');
-        }
-
+        lesson.classList.toggle('active');
     });
 });
 
